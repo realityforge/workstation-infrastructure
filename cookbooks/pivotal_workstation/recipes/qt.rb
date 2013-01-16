99aec1dc27c065011304b24e53ed709a0f5bf4f8
Peter Donald <peter@realityforge.org>
2013-01-16 11:25:21 +1100

    Braid in some sample repositories

---
+++ cookbooks/pivotal_workstation/recipes/qt.rb
@@ -0,0 +1,4 @@
+include_recipe "pivotal_workstation::homebrew"
+include_recipe "pivotal_workstation::xquartz"
+
+brew_install "qt"
---
+++ cookbooks/roderik_workstation/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Roderik van der Veer"
+maintainer_email  "roderik@vanderveer.be"
+license           "MIT"
+description       "My personal settings"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0"
+supports          "mac_os_x"
+
+depends			  "pivotal_workstation"
