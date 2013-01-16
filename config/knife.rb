0425c520eb7432b6d533e6e448dfd9ee280bd15e
Peter Donald <peter@realityforge.org>
2013-01-16 11:56:38 +1100

    Add in chef configuration files

---
+++ config/knife.rb
@@ -0,0 +1,4 @@
+cookbook_path       File.expand_path(File.join(File.dirname(__FILE__), "..","cookbooks"))
+cookbook_copyright  "Peter Donald"
+cookbook_email      "peter@realityforge.org"
+cookbook_license    "apachev2"
