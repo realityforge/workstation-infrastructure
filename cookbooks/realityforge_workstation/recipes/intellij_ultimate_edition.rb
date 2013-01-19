bb92ac93f5fafe2f2017147f2a6fcf8db4ba81da
Peter Donald <peter@realityforge.org>
2013-01-19 17:12:55 +1100

    Add in code style configuration

--- cookbooks/realityforge_workstation/recipes/intellij_ultimate_edition.rb
+++ cookbooks/realityforge_workstation/recipes/intellij_ultimate_edition.rb
@@ -4,6 +4,26 @@ dmg_package "IntelliJ IDEA 12" do
   action :install
 end
 
+base_directory = "#{REAL_HOME}/Library/Preferences/IntelliJIdea12"
+directory base_directory do
+  owner REAL_USER
+  group REAL_GROUP
+  mode "0700"
+end
+
+directory "#{base_directory}/codestyles" do
+  owner REAL_USER
+  group REAL_GROUP
+  mode "0700"
+end
+
+template "#{base_directory}/codestyles/StockSoftware.xml" do
+  source "StockSoftware.xml.erb"
+  owner REAL_USER
+  group REAL_GROUP
+  mode "0600"
+end
+
 template "/Applications/IntelliJ IDEA 12.app/bin/idea.vmoptions" do
   source "idea.vmoptions.erb"
   owner REAL_USER
