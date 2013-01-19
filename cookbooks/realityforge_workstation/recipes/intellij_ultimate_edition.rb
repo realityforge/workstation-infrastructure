a4c3b742d88e041fb126c015a78a02eca6fd5db3
Peter Donald <peter@realityforge.org>
2013-01-19 17:12:38 +1100

    Ensure the vmoptions config file has enough memory

--- cookbooks/realityforge_workstation/recipes/intellij_ultimate_edition.rb
+++ cookbooks/realityforge_workstation/recipes/intellij_ultimate_edition.rb
@@ -3,3 +3,10 @@ dmg_package "IntelliJ IDEA 12" do
   owner REAL_USER
   action :install
 end
+
+template "/Applications/IntelliJ IDEA 12.app/bin/idea.vmoptions" do
+  source "idea.vmoptions.erb"
+  owner REAL_USER
+  group REAL_GROUP
+  mode "0600"
+end
